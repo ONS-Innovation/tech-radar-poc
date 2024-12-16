4ec14b24bcd4518fa53b550517ee9359cb2e3df4
Sebastian <Sebastian.Evison-Theodossiades@ons.gov.uk>
2024-12-16 09:23:14 +0000

    Update .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+      browser: true,
+      es2021: true,
+      jest: true,
     },
-  },
-  plugins: ['react', 'react-hooks'],
-  settings: {
-    react: {
-      version: 'detect',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-duplicate-imports': 'error',
-    'no-multiple-empty-lines': ['error', { max: 1 }],
-  },
-}; 
+    plugins: ['react', 'react-hooks'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-duplicate-imports': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-undef': 'off',
+    },
+  }; 
