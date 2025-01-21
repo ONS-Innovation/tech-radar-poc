7ea7dd66250b6d060c8b021b70e3c91f18dedc96
Sebastian <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-01-21 11:38:06 +0000

    missing s3client

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@
 const express = require("express");
 const cors = require("cors");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const fetch = require("node-fetch");
 const Papa = require("papaparse");
 
